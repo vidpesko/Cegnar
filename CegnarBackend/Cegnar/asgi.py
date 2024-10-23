ef38922cf6fd0215255942fe74fd86b44a5ae14c
Vid Pesko <vid@pesko.si>
2024-10-23 19:02:06 +0200

    Initial upload 2

--- CegnarBackend/Cegnar/asgi.py
+++ CegnarBackend/Cegnar/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for CegnarBackend project.
+ASGI config for Cegnar project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'CegnarBackend.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Cegnar.settings')
 
 application = get_asgi_application()
--- CegnarBackend/Cegnar/urls.py
+++ CegnarBackend/Cegnar/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for CegnarBackend project.
+URL configuration for Cegnar project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/5.1/topics/http/urls/
