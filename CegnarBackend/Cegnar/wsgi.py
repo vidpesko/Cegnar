a48241fbfebf4e5c4062efab7adeb11446655f4c
Vid Pesko <vid@pesko.si>
2024-11-15 00:07:02 +0100

    test wsgi 2

--- CegnarBackend/Cegnar/wsgi.py
+++ CegnarBackend/Cegnar/wsgi.py
@@ -7,15 +7,10 @@ For more information on this file, see
 https://docs.djangoproject.com/en/5.0/howto/deployment/wsgi/
 """
 
-# import os
+import os
 
-# from django.core.wsgi import get_wsgi_application
+from django.core.wsgi import get_wsgi_application
 
-# os.environ.setdefault("DJANGO_SETTINGS_MODULE", "Cegnar.settings.dev")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "Cegnar.settings.dev")
 
-# application = get_wsgi_application()
-
-
-def application(env, start_response):
-    start_response("200 OK", [("Content-Type", "text/html")])
-    return [b"Hello World"]
+application = get_wsgi_application()
