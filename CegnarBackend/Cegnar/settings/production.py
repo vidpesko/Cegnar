7af825bd7166884b1140ded3d614205fa3e02557
Vid Pesko <vid@pesko.si>
2024-11-17 11:50:56 +0100

    Changed production settings WAGTAILADMIN_BASE_URL

--- CegnarBackend/Cegnar/settings/production.py
+++ CegnarBackend/Cegnar/settings/production.py
@@ -22,6 +22,8 @@ PARENT_DIR = os.path.join(os.path.dirname(os.path.dirname(BASE_DIR)), "public_ht
 STATIC_ROOT = os.path.join(PARENT_DIR, "static")
 MEDIA_ROOT = os.path.join(PARENT_DIR, "media")
 
+WAGTAILADMIN_BASE_URL = "https://cegnarblacksmithing.com/backend/"
+
 
 try:
     from .local import *
