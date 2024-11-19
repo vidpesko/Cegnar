fd6d83545e2a1151c61573dec62b6e07d078c9cd
Vid Pesko <vid@pesko.si>
2024-11-19 01:35:26 +0100

    Added image description to GalleryImage component

--- CegnarFrontend/src/lib/components/GalleryImage.svelte
+++ CegnarFrontend/src/lib/components/GalleryImage.svelte
@@ -11,9 +11,9 @@
     <!-- svelte-ignore a11y_click_events_have_key_events -->
     <!-- svelte-ignore a11y_no_static_element_interactions -->
     <div on:click={() => openModal(product)} class="cursor-pointer text-white absolute z-10 bottom-0 h-full w-full bg-opacity-70 text-center py-4 opacity-0" style="background: linear-gradient(180deg, rgba(0,0,0,0) 20%, rgba(0,0,0,0.8) 50%);">
-        <div class="absolute bottom-[10%] text-center w-full">
+        <div class="absolute bottom-[10%] text-center w-full px-5">
             <h4 class="text-xl font-bold mb-2">{product.name}</h4>
-            <p class="mb-8">{@html product.description}</p>
+            <p class="mb-8">{@html product.image_description}</p>
             <a href="/kontakt?izdelek={product.id}" data-sveltekit-reload class="btn py-4 hidden md:inline-block">Naroci tak noz</a>
         </div>
     </div>
